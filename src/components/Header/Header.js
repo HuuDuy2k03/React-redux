da753d19074a9e2949e23af07cebe293580aff47
beckham2k3 <nguyenhuud554@gmail.com>
2025-08-18 19:11:59 +0700

    index router with reactive-router

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">QWQ</Navbar.Brand>
+        <Navbar.Brand as={NavLink} to="/">QWQ</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
---
+++ src/components/Home/HomePage.js
@@ -0,0 +1,9 @@
+const HomePage = (props) => {
+  return (
+    <div className="HomePage-container">
+      <h1>Welcome to the Home Page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
