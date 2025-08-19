695cd4404d13d789b82dbc6298e7151cb73005d3
beckham2k3 <nguyenhuud554@gmail.com>
2025-08-19 09:33:54 +0700

    design video

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,9 +1,11 @@
+import video from '../../assets/video-homepage.mp4'; 
 const HomePage = (props) => {
   return (
-    <div className="HomePage-container">
-      <h1>Welcome to the Home Page</h1>
+    <div className="homepage-container">
+      <video autoPlay loop muted>
+        <source src={video} type="video/mp4" />
+      </video>
     </div>
   );
 };
-
 export default HomePage;
