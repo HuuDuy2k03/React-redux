4d9d13c5686961d3a215f2cbd3ea7d7bf978941f
beckham2k3 <nguyenhuud554@gmail.com>
2025-09-10 21:54:07 +0700

    Design dashboard

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -3,6 +3,8 @@ import './Admin.scss'; // Assuming you have some styles for the Admin component
 import { FaBars } from 'react-icons/fa';
 import React, { useState } from 'react';
 import { Outlet } from "react-router-dom";
+import { NavDropdown } from "react-bootstrap";
+import Language from "../Header/Languages";
 
 const Admin = (props) => {
   const [collapsed, setCollapsed] = useState(false);
@@ -24,8 +26,15 @@ const Admin = (props) => {
       </div>
       <div className="admin-content">
         <div className="admin-header">
-          <FaBars onClick={handleToggleSidebar} />
-          Admin Component
+          <span onClick={handleToggleSidebar}><FaBars className="left-side"/></span>
+          <div className="right-side">
+            <NavDropdown title="Settings"  id="basic-nav-dropdown">
+                <NavDropdown.Item >Profile</NavDropdown.Item>
+                <NavDropdown.Item >Logout</NavDropdown.Item>
+            </NavDropdown>
+            <Language/>
+          </div>
+          
         </div>
         <div className="admin-main">
           <Outlet />
